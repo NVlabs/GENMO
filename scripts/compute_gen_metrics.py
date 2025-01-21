ecd0f7bf48231d148bfe86bbf95830ba65851adc
noahcao <jinkuncao@gmail.com>
2025-01-20 17:01:05 -0800

    add gen metrics in a single file

---
+++ scripts/compute_gen_metrics.py
@@ -0,0 +1,218 @@
+import numpy as np
+from scipy import linalg
+import torch 
+from collections import OrderedDict
+
+# from action2motion
+
+def evaluate_matching_score(eval_wrapper, motion_loaders, file):
+    match_score_dict = OrderedDict({})
+    R_precision_dict = OrderedDict({})
+    activation_dict = OrderedDict({})
+    print('========== Evaluating Matching Score ==========')
+    for motion_loader_name, motion_loader in motion_loaders.items():
+        all_motion_embeddings = []
+        score_list = []
+        all_size = 0
+        matching_score_sum = 0
+        top_k_count = 0
+        # print(motion_loader_name)
+        with torch.no_grad():
+            for idx, batch in enumerate(motion_loader):
+                word_embeddings, pos_one_hots, _, sent_lens, motions, m_lens, _ = batch
+                text_embeddings, motion_embeddings = eval_wrapper.get_co_embeddings(
+                    word_embs=word_embeddings,
+                    pos_ohot=pos_one_hots,
+                    cap_lens=sent_lens,
+                    motions=motions,
+                    m_lens=m_lens
+                )
+                dist_mat = euclidean_distance_matrix(text_embeddings.cpu().numpy(),
+                                                     motion_embeddings.cpu().numpy())
+                matching_score_sum += dist_mat.trace()
+
+                argsmax = np.argsort(dist_mat, axis=1)
+                top_k_mat = calculate_top_k(argsmax, top_k=3)
+                top_k_count += top_k_mat.sum(axis=0)
+
+                all_size += text_embeddings.shape[0]
+
+                all_motion_embeddings.append(motion_embeddings.cpu().numpy())
+
+            all_motion_embeddings = np.concatenate(all_motion_embeddings, axis=0)
+            matching_score = matching_score_sum / all_size
+            R_precision = top_k_count / all_size
+            match_score_dict[motion_loader_name] = matching_score
+            R_precision_dict[motion_loader_name] = R_precision
+            activation_dict[motion_loader_name] = all_motion_embeddings
+
+        print(f'---> [{motion_loader_name}] Matching Score: {matching_score:.4f}')
+        print(f'---> [{motion_loader_name}] Matching Score: {matching_score:.4f}', file=file, flush=True)
+
+        line = f'---> [{motion_loader_name}] R_precision: '
+        for i in range(len(R_precision)):
+            line += '(top %d): %.4f ' % (i+1, R_precision[i])
+        print(line)
+        print(line, file=file, flush=True)
+
+    return match_score_dict, R_precision_dict, activation_dict
+
+
+def calculate_fid(statistics_1, statistics_2):
+    return calculate_frechet_distance(statistics_1[0], statistics_1[1],
+                                      statistics_2[0], statistics_2[1])
+
+
+def calculate_activation_statistics(activations):
+    """
+    Params:
+    -- activation: num_samples x dim_feat
+    Returns:
+    -- mu: dim_featcccccbkvecgrfrjgrctkkhtnnjcbctdvhktfgbergbnh
+    -- sigma: dim_feat x dim_feat
+    """
+    mu = np.mean(activations, axis=0)
+    cov = np.cov(activations, rowvar=False)
+    return mu, cov
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    Stable version by Dougal J. Sutherland.
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+    Returns:
+    --   : The Frechet Distance.
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
+
+
+def calculate_diversity(activation, diversity_times):
+    assert len(activation.shape) == 2
+    assert activation.shape[0] > diversity_times
+    num_samples = activation.shape[0]
+
+    first_indices = np.random.choice(num_samples, diversity_times, replace=False)
+    second_indices = np.random.choice(num_samples, diversity_times, replace=False)
+    dist = linalg.norm(activation[first_indices] - activation[second_indices], axis=1)
+    return dist.mean()
+
+def evaluate_diversity(activation_dict, file, diversity_times=300):
+    eval_dict = OrderedDict({})
+    print('========== Evaluating Diversity ==========')
+    for model_name, motion_embeddings in activation_dict.items():
+        diversity = calculate_diversity(motion_embeddings, diversity_times)
+        eval_dict[model_name] = diversity
+        print(f'---> [{model_name}] Diversity: {diversity:.4f}')
+        print(f'---> [{model_name}] Diversity: {diversity:.4f}', file=file, flush=True)
+    return eval_dict
+
+def calculate_multimodality(activation, multimodality_times):
+    assert len(activation.shape) == 3
+    assert activation.shape[1] > multimodality_times
+    num_per_sent = activation.shape[1]
+
+    first_dices = np.random.choice(num_per_sent, multimodality_times, replace=False)
+    second_dices = np.random.choice(num_per_sent, multimodality_times, replace=False)
+    dist = linalg.norm(activation[:, first_dices] - activation[:, second_dices], axis=2)
+    return dist.mean()
+
+
+def evaluate_multimodality(eval_wrapper, mm_motion_loaders, file, mm_num_times=0):
+    eval_dict = OrderedDict({})
+    print('========== Evaluating MultiModality ==========')
+    for model_name, mm_motion_loader in mm_motion_loaders.items():
+        mm_motion_embeddings = []
+        with torch.no_grad():
+            for idx, batch in enumerate(mm_motion_loader):
+                # (1, mm_replications, dim_pos)
+                motions, m_lens = batch
+                motion_embedings = eval_wrapper.get_motion_embeddings(motions[0], m_lens[0])
+                mm_motion_embeddings.append(motion_embedings.unsqueeze(0))
+        if len(mm_motion_embeddings) == 0:
+            multimodality = 0
+        else:
+            mm_motion_embeddings = torch.cat(mm_motion_embeddings, dim=0).cpu().numpy()
+            multimodality = calculate_multimodality(mm_motion_embeddings, mm_num_times)
+        print(f'---> [{model_name}] Multimodality: {multimodality:.4f}')
+        print(f'---> [{model_name}] Multimodality: {multimodality:.4f}', file=file, flush=True)
+        eval_dict[model_name] = multimodality
+    return eval_dict
+
+
+# GT_vectors_path = 'outputs/humanml3d_feats_gt/feats_test.pt'
+# Pred_vectors_path = 'outputs/mocap_mixed_v1/unimfm/unimfm_test_st_g8/version_0/text_feats/feats.pt'
+
+# GT_vectors_path = 'outputs/humanml3d_feats_gt/feats_test_humanml3d_format.pt.npy'
+# Pred_vectors_path = 'outputs/mocap_mixed_v1/unimfm/unimfm_test_st_g8/version_0/text_feats/feats_humanml3d_format.npy'
+GT_vectors_path = 'outputs/ground_truth_motions.npy'
+Pred_vectors_path = 'outputs/mdm_vald_motions.npy'
+
+
+GT_feats = np.load(GT_vectors_path)
+Pred_feats = np.load(Pred_vectors_path)
+
+mean = np.load('outputs/humanml3d_mean.npy')
+std = np.load('outputs/humanml3d_std.npy')
+GT_feats = GT_feats * std + mean
+Pred_feats = Pred_feats * std + mean
+
+feat_dim = GT_feats.shape[-1]
+GT_feats = GT_feats.reshape(-1, feat_dim)
+Pred_feats = Pred_feats.reshape(-1, feat_dim)
+
+
+def compute_fid(feats1, feats2):
+    mu1, sigma1 = calculate_activation_statistics(feats1)
+    mu2, sigma2 = calculate_activation_statistics(feats2)
+    fid = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
+    return fid
+
+fid = compute_fid(GT_feats, Pred_feats)
+
+
+
+# FID:
+# mdm: 1.7330507772884864
+# ours: 0.13503613421584149
