3d70e382f7f12f1b11ceccdf6659ded26c4347e8
noahcao <jinkuncao@gmail.com>
2025-01-22 10:09:40 -0800

    add script for motionx label saving

---
+++ scripts/save_motionx_gt.py
@@ -0,0 +1,32 @@
+import sys 
+import os 
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+import pickle 
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from hmr4d.dataset.motionx.motionx import MotionXDataset
+from hmr4d.model.gvhmr.utils.endecoder import EnDecoder
+from motiondiff.utils.torch_utils import tensor_to
+
+encoder = EnDecoder(stats_name='DEFAULT_01', encode_type='humanml3d').cuda()
+
+test_dataset = MotionXDataset(split='test', version='vlocal', max_motion_frames=196)
+dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0)
+
+texts = []
+motions = []
+
+for data in tqdm(dataloader):
+    print(data['smpl_params_w']['body_pose'].shape)
+    data = tensor_to(data, 'cuda')
+    x = encoder.encode_humanml3d(data)
+    motions.append(x)
+    texts.append(data['caption'])
+
+with open('inputs/motionx_gt_feats.pkl', 'wb') as f:
+    pickle.dump((texts, motions), f)
+breakpoint()
+
