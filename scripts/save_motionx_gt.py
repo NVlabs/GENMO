066e8ee54029bfc5d6e4ba281b9aaf98a8e8196f
noahcao <jinkuncao@gmail.com>
2025-01-22 11:54:02 -0800

    save motionx gt

--- scripts/save_motionx_gt.py
+++ scripts/save_motionx_gt.py
@@ -3,6 +3,7 @@ import os
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 import pickle 
+import torch
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
@@ -13,20 +14,24 @@ from motiondiff.utils.torch_utils import tensor_to
 
 encoder = EnDecoder(stats_name='DEFAULT_01', encode_type='humanml3d').cuda()
 
-test_dataset = MotionXDataset(split='test', version='vlocal', max_motion_frames=196)
+test_dataset = MotionXDataset(split='test', version='vlocal', max_motion_frames=196, motion_start_mode='fixed')
 dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0)
 
 texts = []
 motions = []
 
 for data in tqdm(dataloader):
-    print(data['smpl_params_w']['body_pose'].shape)
     data = tensor_to(data, 'cuda')
     x = encoder.encode_humanml3d(data)
     motions.append(x)
     texts.append(data['caption'])
+  
+motions = torch.cat(motions, dim=0).cpu().numpy()
+
+result_dict = {'motions': motions, 'texts': texts}
 
 with open('inputs/motionx_gt_feats.pkl', 'wb') as f:
-    pickle.dump((texts, motions), f)
-breakpoint()
+    pickle.dump(result_dict, f)
+
+
 
